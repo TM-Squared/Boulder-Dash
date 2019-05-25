e52ebdd6b837da692524b03239c2c9940cea2188
10Saavage <44948364+10Saavage@users.noreply.github.com>
2019-05-25 15:21:18 +0100

    phase1

---
+++ Boulderdash/controller/src/main/java/Boulderdash/controller/App.java
@@ -0,0 +1,13 @@
+package Boulderdash.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Boulderdash/view/src/main/java/Boulderdash/view/App.java
@@ -0,0 +1,13 @@
+package Boulderdash.view;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
